19b8c67ee69e2ce4b635a589a355f96f880cac7e
dldkffh <79070302+dldkffh@users.noreply.github.com>
2023-07-09 14:29:45 +0900

    test adfit

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useBlogPost} from '@docusaurus/theme-common/internal';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import GiscusComponent from '../../components/GiscusComponent';
+import kakaoAdComponents from '../../components/kakaoAdComponents';
 
 export default function BlogPostItemWrapper(props) {
   const {metadata, isBlogPostPage} = useBlogPost();
@@ -12,9 +13,8 @@ export default function BlogPostItemWrapper(props) {
   return (
     <>
       <BlogPostItem {...props} />
-      {enableComments && isBlogPostPage && (
-        <GiscusComponent />
-      )}
+      {isBlogPostPage && <kakaoAdComponents />}
+      {enableComments && isBlogPostPage && <GiscusComponent />}
     </>
   );
 }
