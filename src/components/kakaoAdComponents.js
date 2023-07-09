342a79c828d394a5aa959db071c995d09b320c98
dldkffh <79070302+dldkffh@users.noreply.github.com>
2023-07-09 14:41:26 +0900

    test ad

--- src/components/kakaoAdComponents.js
+++ src/components/kakaoAdComponents.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const html = `<ins
-class="kakao_ad_area"
-style="display:none;"
-data-ad-unit="DAN-YNC5GGYBYGAah2B4"
-data-ad-width="250"
-data-ad-height="250"></ins>
-<script
-type="text/javascript"
-src="//t1.daumcdn.net/kas/static/ba.min.js"
-async></script>`;
-
 export default function kakaoAdComponents() {
-  return <div dangerouslySetInnerHTML={{__html: html}}></div>;
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `<ins
+    class="kakao_ad_area"
+    style="display:none;"
+    data-ad-unit="DAN-YNC5GGYBYGAah2B4"
+    data-ad-width="250"
+    data-ad-height="250"></ins>
+    <script
+    type="text/javascript"
+    src="//t1.daumcdn.net/kas/static/ba.min.js"
+    async></script>`,
+      }}></div>
+  );
 }
